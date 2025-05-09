d6ff5b9f9a2abca2345c18b69b175c5439f58865
Tryna <juangiraldocx@example.com>
2025-05-09 15:41:35 -0500

    Adición de provider, modelo de video y tema

---
+++ toktik/lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Color _myGreen = Color(0xFF368f4f);
+List<Color> _colorTheme = [
+  _myGreen,
+  Colors.green[300]!,
+  Colors.black54,
+  Colors.lightBlueAccent,
+  Colors.blueGrey,
+];
+
+class AppTheme {
+
+  final int selectedColorIndex;
+
+  AppTheme({this.selectedColorIndex = 0}) : assert(
+    selectedColorIndex >= 0 && selectedColorIndex <= _colorTheme.length - 1,
+    'selectedColorIndex must be between 0 and ${_colorTheme.length - 1}',
+  );
+
+  ThemeData getTheme() {
+    return ThemeData(
+      colorSchemeSeed: _colorTheme[selectedColorIndex],
+      brightness: Brightness.dark,
+    );
+  }
+}
