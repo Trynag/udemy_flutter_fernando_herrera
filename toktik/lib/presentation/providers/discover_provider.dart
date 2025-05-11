71b8be586af7f5429d53cb7d19bc7a2a3d736e5b
Tryna <juangiraldocx@example.com>
2025-05-10 23:26:18 -0500

    Add PageView widget

--- toktik/lib/presentation/providers/discover_provider.dart
+++ toktik/lib/presentation/providers/discover_provider.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   
@@ -7,8 +10,15 @@ class DiscoverProvider extends ChangeNotifier {
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // TODO: load videos
 
+    await Future.delayed( const Duration(seconds: 2) );
+
+    final List<VideoPost> newVideos = videoPosts.map( 
+      (video) => LocalVideoModel.fromJSON(video).toVideoPostEntity() 
+    ).toList();
+
+    videos.addAll( newVideos) ;
+    initialLoading = false;
     notifyListeners();
   }
 }
